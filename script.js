73473cbff8383cb1bd90e2a68b6be94cbc6187eb
pips1804 <jhonpaulmanlapaz1804@gmail.com>
2024-10-03 06:58:51 +0800

    done portfolio

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 function toggleTheme() {
   const body = document.body;
+  const progress = document.querySelectorAll(".container");
+  const card = document.querySelectorAll(".card");
+  const myImage = document.getElementById("profile");
+
   if (body.classList.contains("dark-mode")) {
     body.classList.remove("dark-mode");
     body.classList.add("light-mode");
+    progress.forEach((container) => {
+      container.style.backgroundColor = "#222";
+    });
+    myImage.src = "assets/images/me.jpg";
+
+    card.forEach((container) => {
+      container.style.backgroundColor = "#222";
+    });
   } else {
     body.classList.remove("light-mode");
     body.classList.add("dark-mode");
+    progress.forEach((container) => {
+      container.style.backgroundColor = "#111";
+    });
+
+    card.forEach((container) => {
+      container.style.backgroundColor = "#111";
+    });
+    myImage.src = "assets/images/me2.jpg";
   }
 }
 
